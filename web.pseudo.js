c13959bebc71c659425452d2cc3a1eb5acd2fff4
Aza Raskin <aza@air.local>
2016-06-12 14:19:00 -0700

    Updating web bundle to the new style and added thoughts
    When you click the 'go' action, the browser opened should allow you
    to selecting text and then post the equiv of a screenshot+link. This
    lets you talk about things on the web, link them with context, all
    without leaving Other Chat. Think a much better version of the web
    snippets thing on Twitter.

--- web.pseudo.js
+++ web.pseudo.js
@@ -1,7 +1,22 @@
-var otherchat = new Otherchat(),
+//
+// WEB
+// Either does a web search or opens a URL
+//
+
+// When you click the 'go' action, the browser opened should allow you
+// to selecting text and then post the equiv of a screenshot+link. This
+// lets you talk about things on the web, link them with context, all
+// without leaving Other Chat. Think a much better version of the web
+// snippets thing on Twitter.
+
+var feature = new OtherchatFeature({
+      apiKey: 'cdb6b77b-99c3-454e-8e89-185badc4644e',
+      version: 'web-bundle.0.1'
+    })
+    otherchat = new Otherchat( feature ),
     Web = require('other-web') // Extended-local web search?
 
-var cmd = OtherchatFeature({
+var cmd = feature.command({
   tokens: ['web'],
   version: 'web.0.1',
   apiKey: 'cdb6b77b-99c3-454e-8e89-185badc4644e'
@@ -9,11 +24,11 @@ var cmd = OtherchatFeature({
 
 cmd.on('query', (context, promise) => {
 
-  try{
+  try {
 
     var results = await Web.search( context.query )
     results = results.map( page => {
-      return oc.types.twoLineChatComplete({
+      return oc.types.chatComplete({
         title: page.title,
         detail: page.href,
         actionName: 'go',
@@ -27,8 +42,6 @@ cmd.on('query', (context, promise) => {
 
   }
 
-  catch (error) {
-    promise.reject( `Something went wrong: ${error}` )
-  }
+  catch (error) promise.reject( error )
   
 })
