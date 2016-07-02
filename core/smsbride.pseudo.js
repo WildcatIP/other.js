24e40b3537b45eef180a5c8ef3643d218d523f41
Aza <aza@Azas-MacBook.local>
2016-07-02 15:41:54 -0700

    Some old early thoughts on the Sms Bride

--- core/smsbride.pseudo.js
+++ core/smsbride.pseudo.js
@@ -1,24 +1,53 @@
-var otherchat = new Otherchat(),
-    server = otherchat.server,
-    Twilio = require('...')
+var feature = new FeatureSet({
+  apiKey: 'cdb6b77b-99c3-454e-8e89-185badc4644e', // root ;)
+  id: 'smsbride',
+  version: '0.1',
+  name: 'SMS Bride nOG'
+})
 
-function isValidPhoneNumber( string ){
-  /* code */
-}
-    
-server.channel('*').on('message', function(message){
+// Twilio Credentials 
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authToken = '[AuthToken]',
+    twilio = require('twilio')(accountSid, authToken)
+
+
+// Other Chat
+var otherchat = new Otherchat( feature ),
+    phoneRegexp = /.../
+
+// Find all at mentions that are phone numbers, and text them
+
+otherchat.client.on( 'willPostMessage', (context, postMessage) => {
+
+  var message = context.message,
+      phoneUsers = message.userMentions.filter( mention => mention.name.match( phoneRegexp ) )
+
   
-  var phoneUsers = message.userMentions.each(function( user ){
-    
-    if( isPhoneNumber(user.name.match) ){
-      Twilio.send({
+  // After a user is created this way, you just nickname them to a mneumonic. The sms icon shows.
+  var smsPromises = phoneUsers.map( user => {
+
+    return new Promise( (reject, resolve) => {
+
+      var endpoint = otherchat.server.Endpoint()
+      
+      twilio.messages.create({
         to: user.name,
-        body: [ message.from, ':\n', message.text ]
+        from: '+14153325538',
+        body: message.text,
+        StatusCallback: endpoint.url
       })
-    }
+
+      endpoint.on('post', request => {
+        if( request.params.MessageStatus == 'sent' ) resolve( true )
+        if( request.params.MessageStats == 'failed' ) reject( request.params.ErrorCode )
+      })
+
+    })
 
   })
 
-})
+  Promise.all( smsPromises )
+    .then( () => postMessage.resolve( true ) )
+    .catch( reason => postMessage.reject( reason ) )
 
-// TODO: The bridge back
+})
