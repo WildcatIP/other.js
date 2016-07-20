5256de318482e984626be7ca9337b6cdcb1d99ab
Tony Gentilcore <tony@other.xyz>
2016-07-20 08:20:37 -0700

    Add an npm package and teach it to lint examples.

--- examples/donger.pseudo.js
+++ examples/donger.pseudo.js
@@ -1,39 +1,37 @@
-var DONGERS = [
-	"⊂(▀¯▀⊂)",
-	"ᕙ(˵ ಠ ਊ ಠ ˵)ᕗ",
-	"ԅ( ͒ ۝ ͒ )ᕤ",
-	"o͡͡͡╮༼ • ʖ̯ • ༽╭o͡͡͡",
-	"ᕙʕ ಥ ▃ ಥ ʔᕗ",
-	"ᕙᓄ(☉ਊ☉)ᓄᕗ",
-	"╰། ◉ ◯ ◉ །╯",
-	"ʕ ᓀ ᴥ ᓂ ʔ",
-	"ᕦ༼ ✖ ਊ ✖ ༽ᕤ",
-	"ლ(ಥ Д ಥ )ლ",
-	"⋌༼ •̀ ⌂ •́ ༽⋋",
-	"( ຈ ﹏ ຈ )",
-	"¯\\_༽ ಥ Д ಥ ༼_/¯",
-	"໒( •̀ ╭ ͟ʖ╮ •́ )७",
-	"[ * ༎ຶ _ ༎ຶ * ]"
-]
+const DONGERS = [
+  "⊂(▀¯▀⊂)",
+  "ᕙ(˵ ಠ ਊ ಠ ˵)ᕗ",
+  "ԅ( ͒ ۝ ͒ )ᕤ",
+  "o͡͡͡╮༼ • ʖ̯ • ༽╭o͡͡͡",
+  "ᕙʕ ಥ ▃ ಥ ʔᕗ",
+  "ᕙᓄ(☉ਊ☉)ᓄᕗ",
+  "╰། ◉ ◯ ◉ །╯",
+  "ʕ ᓀ ᴥ ᓂ ʔ",
+  "ᕦ༼ ✖ ਊ ✖ ༽ᕤ",
+  "ლ(ಥ Д ಥ )ლ",
+  "⋌༼ •̀ ⌂ •́ ༽⋋",
+  "( ຈ ﹏ ຈ )",
+  "¯\\_༽ ಥ Д ಥ ༼_/¯",
+  "໒( •̀ ╭ ͟ʖ╮ •́ )७",
+  "[ * ༎ຶ _ ༎ຶ * ]"
+];
 
-let feature = new Feature({ id: 'donger.0.1' })
+const feature = new Feature({id: 'donger.0.1'});
 
-var command = feature.command({
-    tokens: [ 'donger' ],
-    accepts: { query: String }
-})
+const command = feature.command({
+  tokens: ['donger'],
+  accepts: {query: String}
+});
 
 command.on('didQuery', (context, completes) => {
+  const {query} = context;
+  let shuffledDongers = DONGERS.sort(() => 0.5 - Math.random());
 
-	var shuffledDongers = DONGERS.sort( () => 0.5 - Math.random() )
+  if (query.length > 1) {
+    const sampleSize = Math.round(DONGERS.length / Math.pow(query.length, 2));
+    shuffledDongers = shuffledDongers.slice(0, sampleSize);
+  }
 
-	if( context.query.length > 1 ){
-		var sampleSize = Math.round( DONGERS.length / Math.pow( query.length, 2 ) )
-		shuffledDongers = shuffledDongers.slice( 0, sampleSize )
-	}
-
-	var results = shuffledDongers.map( donger => ({ text: donger }) )
-
-	completes.resolve( results )
-
-})
+  const results = shuffledDongers.map(donger => ({text: donger}));
+  completes.resolve(results);
+});
