173e4f01ed5756455dea929b91083c3febed206d
Tony Gentilcore <tony@other.xyz>
2016-07-21 21:12:43 -0700

    Generate an other.min.js for browsers.
    Ref #3

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const validate = require('webpack-validator');
+const webpack = require('webpack');
+
+const isProd = (process.env.NODE_ENV === 'production');
+
+function getPlugins() {
+  const plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    })
+  ];
+  if (isProd) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: {warnings: false}
+      // TODO: Mangling reduces bundle size by 20%, but breaks things.
+      // Need to figure out the right set of exceptions.
+      // mangle: {props: true}
+    }));
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+  }
+  return plugins;
+}
+
+module.exports = validate({
+  devtool: isProd ? 'source-map' : 'eval-source-map',
+  entry: {
+    other: './other.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './other.min.js',
+    sourceMapFilename: "./[name].js.map"
+  },
+  plugins: getPlugins()
+});
