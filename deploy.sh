d2eae3ca574275a6185b057c1bc023a6d0bd932c
Tony Gentilcore <tony@other.xyz>
2016-08-10 16:53:00 -0700

    Test a hardcoded redirect command.
    Ref #13

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 set -ex
 
 aws --region=us-west-2 s3 sync --acl public-read --exclude ".*" ./dist/ s3://apps.other.chat/
+aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/0.0.x/other.min.js --website-redirect-location otherjs/0.0.1+3880ffe/other.min.js
