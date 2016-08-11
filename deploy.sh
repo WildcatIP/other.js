cfc4360b525a14bdf9fb136c6999ce5cc6bd3ee3
Tony Gentilcore <tony@other.xyz>
2016-08-10 18:18:05 -0700

    Cache redirects for 24 hours.
    Ref #13

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ set -ex
 aws --region=us-west-2 s3 sync --acl public-read --exclude ".*" ./dist/ s3://apps.other.chat/
 
 # Create semantic version redirects.
-# TODO: Cache for 24hrs.
 VERSION=`grep -o '"version": "[^"]*"' package.json | cut -d'"' -f4`
 MAJOR=`echo $VERSION | cut -d. -f1`
 MINOR=`echo $VERSION | cut -d. -f2`
@@ -14,6 +13,6 @@ REVISION=`git rev-parse --short HEAD`
 cd dist/otherjs/$VERSION+$REVISION
 for FILE in *
 do
-  aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/$MAJOR.$MINOR.x/$FILE --website-redirect-location /otherjs/$VERSION+$REVISION/$FILE --content-type application/javascript
-  aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/$MAJOR.x/$FILE --website-redirect-location /otherjs/$VERSION+$REVISION/$FILE --content-type application/javascript
+  aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/$MAJOR.$MINOR.x/$FILE --website-redirect-location /otherjs/$VERSION+$REVISION/$FILE --content-type application/javascript --cache-control "max-age=86400, public"
+  aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/$MAJOR.x/$FILE --website-redirect-location /otherjs/$VERSION+$REVISION/$FILE --content-type application/javascript --cache-control "max-age=86400, public"
 done
