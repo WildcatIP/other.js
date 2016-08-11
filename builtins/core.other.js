f12b9594fbeb9cf7455d4127e6b90e96815afea0
Aza Raskin <azaaza@gmail.com>
2016-08-11 12:22:01 -0700

    Update core.other.js

--- builtins/core.other.js
+++ builtins/core.other.js
@@ -8,6 +8,17 @@ const feature = new Feature({
   }
 })
 
+// Why not do the conversion to Command implicitly, so:
+//   feature.commadns.push({ ... })
+// Same thing for the StagedMessageResult
+
+// tokens and onQuery are vestigial naming from the very first version of oext's. Can we think of better names?
+// (I'm a little into calling tokens "words" [|....] )
+
+// What should "/s" be acutally called. Maybe "/small"?
+
+// Not sure I fully grok yet what a StagedMessageResult is
+
 feature.commands.push(new Command({
   tokens: ['/blockquote', '/caption', '/h1', '/h2', '/h3', '/p', '/s'],
   onQuery(token, query) {
