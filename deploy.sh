b391f3ee7329bb90d67967863834861c71ee4ac3
Tony Gentilcore <tony@other.xyz>
2016-08-10 17:07:47 -0700

    Another attempt at fixing the redirect.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -ex
 
 aws --region=us-west-2 s3 sync --acl public-read --exclude ".*" ./dist/ s3://apps.other.chat/
-aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/0.0.x/other.min.js --website-redirect-location /otherjs/0.0.1+3880ffe/other.min.js
+aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/0.0.x/other.min.js --website-redirect-location /apps.other.chat/otherjs/0.0.1%2B3880ffe/other.min.js --content-type application/javascript
