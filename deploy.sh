b7a5eecfd2c9e6854652ae46bf8c1c0efe157d14
Tony Gentilcore <tony@other.xyz>
2016-08-29 14:11:49 -0700

    Create symlink for specifying patch version component
    Fixes https://github.com/other-xyz/other-chat-web/issues/179

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,12 @@ aws --region=us-west-2 s3 sync --acl public-read --cache-control "max-age=315360
 VERSION=$(grep -o '"version": "[^"]*"' package.json | cut -d'"' -f4)
 MAJOR=$(echo $VERSION | cut -d. -f1)
 MINOR=$(echo $VERSION | cut -d. -f2)
+PATCH=$(echo $VERSION | cut -d. -f3)
 REVISION=$(git rev-parse --short HEAD)
 cd dist/otherjs/${VERSION}+${REVISION}
 for FILE in *
 do
+  aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/${MAJOR}.${MINOR}.${PATCH}/${FILE} --website-redirect-location /otherjs/${VERSION}+${REVISION}/${FILE} --content-type application/javascript --cache-control "max-age=86400, public"
   aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/${MAJOR}.${MINOR}.x/${FILE} --website-redirect-location /otherjs/${VERSION}+${REVISION}/${FILE} --content-type application/javascript --cache-control "max-age=86400, public"
   aws --region=us-west-2 s3api put-object --bucket apps.other.chat --acl public-read --key otherjs/${MAJOR}.x/${FILE} --website-redirect-location /otherjs/${VERSION}+${REVISION}/${FILE} --content-type application/javascript --cache-control "max-age=86400, public"
 done
