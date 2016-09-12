c58ae4eac93abe237acb066f894a6b5c530b029c
Tony Gentilcore <tony@other.xyz>
2016-09-12 14:16:01 -0700

    Test donger

---
+++ builtins/__tests__/donger.spec.js
@@ -0,0 +1,25 @@
+import {default as donger} from '../donger.other'
+
+describe('donger', () => {
+  beforeEach(() => {
+    spyOn(donger.userAgent, 'emit').and.callThrough()
+  })
+
+  it('ignores messages without donger', done => {
+    donger.userAgent.emit('SET_STAGED_MESSAGE', {message: {text: 'hello world'}, tag: 123})
+    setImmediate(() => {
+      expect(donger.userAgent.emit.calls.count()).toEqual(2)
+      expect(donger.userAgent.emit).toHaveBeenCalledWith('SET_CHAT_COMPLETE_RESULTS', {results: [], replyTag: 123})
+      done()
+    })
+  })
+
+  it('recognizes donger', done => {
+    donger.userAgent.emit('SET_STAGED_MESSAGE', {message: {text: 'donger'}, tag: 123})
+    setImmediate(() => {
+      expect(donger.userAgent.emit.calls.count()).toEqual(2)
+      expect(donger.userAgent.emit.calls.argsFor(1)[1].results.length).toEqual(13)
+      done()
+    })
+  })
+})
