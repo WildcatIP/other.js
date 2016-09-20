b88a66a8dee80a8b96e31815f2c01ca28d554667
Tony Gentilcore <tony@other.xyz>
2016-09-19 17:51:10 -0700

    Deploy the npm package.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -ex
 
+NPM_PACKAGE=$(npm pack)
+
 # Copy all features (cacheable for 1 day).
 aws --region=us-west-2 s3 sync --acl public-read --cache-control "max-age=86400, public" --exclude "otherjs/*" ./dist/ s3://apps.other.chat/
 
 # Copy the library (cachable for 1 year).
 aws --region=us-west-2 s3 sync --acl public-read --cache-control "max-age=31536000, public" ./dist/otherjs/ s3://apps.other.chat/otherjs/
+aws --region=us-west-2 s3 cp --acl public-read --cache-control "max-age=31536000, public" ${NPM_PACKAGE} s3://apps.other.chat/otherjs/
 
 # Create semantic version redirects (cachable for 24 hours).
 VERSION=$(grep -o '"version": "[^"]*"' package.json | cut -d'"' -f4)
