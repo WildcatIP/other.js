9f2604f8ac39c4cf9a565cf5f8008ec2131a739c
Tony Gentilcore <tony@other.xyz>
2016-09-20 15:39:26 -0700

    Make echo a builtin.
    Ref https://github.com/other-xyz/combinator/issues/14

--- builtins/echo.other.js
+++ builtins/echo.other.js
@@ -2,14 +2,14 @@ const {Feature} = require('other')
 
 const feature = new Feature({
   name: 'Echo',
-  version: '0.0.2',
+  version: '0.0.3',
   dependencies: {
-    otherjs: '3.x'
+    otherjs: '3.2.x'
   },
   identity: '5db2ae95f72b4785ae2348d76c463270'
 })
 
-const channel = feature.chatternet.channel('03c2f076c78744dd8e345138ee28ba7f')
+const channel = feature.chatternet.channel({id: '03c2f076c78744dd8e345138ee28ba7f'})
 channel.onReceive(message => channel.send(message))
 
 module.exports = feature
