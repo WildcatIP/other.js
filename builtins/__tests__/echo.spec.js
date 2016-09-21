b77375d59562c14a39e2402328cdb4b5d2cc9155
Tony Gentilcore <tony@other.xyz>
2016-09-21 15:33:04 -0700

    Fix echo to send to the same channel the message was sent to.
    This involved giving it an API where it doesn't have to hardcode the channel
    to send to. At the same time, I decided to make the API look like the other
    feature listeners.
    
    Finally, since these deploy automatically now, add a test.

---
+++ builtins/__tests__/echo.spec.js
@@ -0,0 +1,24 @@
+import {default as echo} from '../echo.other'
+
+describe('echo', () => {
+  beforeEach(() => {
+    spyOn(echo.chatternet, 'emit').and.callThrough()
+  })
+
+  it('echos messages from others', done => {
+    echo.chatternet.emit('UPDATE_MESSAGES', 123, [{text: 'hello', identityId: '234'}])
+    setImmediate(() => {
+      expect(echo.chatternet.emit.calls.count()).toEqual(2)
+      expect(echo.chatternet.emit).toHaveBeenCalledWith('ADD_MESSAGE', 123, {text: 'hello', format: 'system'})
+      done()
+    })
+  })
+
+  it('ignores its own messages', done => {
+    echo.chatternet.emit('UPDATE_MESSAGES', 123, [{text: 'hello', identityId: '5db2ae95f72b4785ae2348d76c463270'}])
+    setImmediate(() => {
+      expect(echo.chatternet.emit.calls.count()).toEqual(1)
+      done()
+    })
+  })
+})
--- builtins/echo.other.js
+++ builtins/echo.other.js
@@ -2,18 +2,20 @@ const {Feature} = require('other')
 
 const feature = new Feature({
   name: 'Echo',
-  version: '0.0.4',
+  version: '0.0.5',
   dependencies: {
     otherjs: '3.2.x'
   },
   identity: '5db2ae95f72b4785ae2348d76c463270'
 })
 
-const channel = feature.chatternet.channel({id: '03c2f076c78744dd8e345138ee28ba7f'})
-channel.onReceive(message => {
-  // Everyone knows that a quack doesn't echo ;-)
-  const text = message.text === 'quack' ? 'silence' : message.text
-  channel.send({format: 'system', text})
+feature.listen({
+  to: 'message',
+  on(message, channel) {
+    // Everyone knows that a quack doesn't echo ;-)
+    const text = message.text === 'quack' ? 'silence' : message.text
+    channel.send({format: 'system', text})
+  }
 })
 
 module.exports = feature
