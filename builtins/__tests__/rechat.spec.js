64791d9299f2c247a87bc38d42228ef69ca6568c
Tony Gentilcore <tony@other.xyz>
2016-09-23 08:24:56 -0700

    De-pseudo rechat-simple to an incorrect (but working) implementation.
    This incorrect implementation works like this:
    
      #original
        [user] /rechat #target hello
    
      #target
        [system] hello
        [system] rechat from #original by @user
    
    There's just a couple of small primitives marked by TODOs that'd let this work
    like rechat-simple intended. This is really just a demo of a cloud feature
    that's more interesting than echo.

---
+++ builtins/__tests__/rechat.spec.js
@@ -0,0 +1,25 @@
+import {default as rechat} from '../rechat.other'
+
+describe('rechat', () => {
+  beforeEach(() => {
+    spyOn(rechat.chatternet, 'emit').and.callThrough()
+  })
+
+  it('does incorrect simplified rechat', done => {
+    rechat.chatternet.emit('UPDATE_MESSAGES', 123, [{text: '/rechat <#03aea5a92a124d679c866e8dc97bdf1d> hello', identityId: '234'}])
+    setImmediate(() => {
+      expect(rechat.chatternet.emit.calls.count()).toEqual(3)
+      expect(rechat.chatternet.emit).toHaveBeenCalledWith('ADD_MESSAGE', '03aea5a92a124d679c866e8dc97bdf1d', {text: 'hello', format: 'system'})
+      expect(rechat.chatternet.emit).toHaveBeenCalledWith('ADD_MESSAGE', '03aea5a92a124d679c866e8dc97bdf1d', {text: 'rechat from <#123> by <@234>', format: 'system'})
+      done()
+    })
+  })
+
+  it('ignores other commands', done => {
+    rechat.chatternet.emit('UPDATE_MESSAGES', 123, [{text: '/something else', identityId: '5db2ae95f72b4785ae2348d76c463270'}])
+    setImmediate(() => {
+      expect(rechat.chatternet.emit.calls.count()).toEqual(1)
+      done()
+    })
+  })
+})
---
+++ builtins/rechat.other.js
@@ -0,0 +1,51 @@
+const {Feature} = require('other')
+
+// This is a stop-gap rechat that we could implement nearly now.
+//
+// Use the rechat command to rechat the last message in a channel. It get's
+// posted by the original author, with a system message beneath it that says:
+//
+// > 'rechat from #foo by @blah'
+const feature = new Feature({
+  name: 'Rechat Simple (Stopgap)',
+  version: '0.0.1',
+  dependencies: {
+    otherjs: '^3.2.x'
+  },
+  identity: '5db2ae95f72b4785ae2348d76c463270'
+})
+
+feature.listen({
+  to: 'message',
+  on(message, channel) {
+    // TODO: Make it possible to get the most recent message. It's incorrect to
+    // "rechat" the rechat message.
+    const {identityId, text} = message
+    const command = parseCommand(text)
+    if (!command) return
+
+    // TODO: Make it possible to send this message on behalf of the user.
+    const targetChannel = feature.chatternet.channel({id: command.channelId})
+    targetChannel.send({format: 'system', text: command.text})
+    targetChannel.send({format: 'system', text: `rechat from <#${channel.id}> by <@${identityId}>`})
+  }
+})
+
+module.exports = feature
+
+// <mess>
+// TODO: This mess all goes away after it's possible to use both a
+// CommandListener and chattetnet events in the same feature.
+const command = '/rechat '
+const channelRegexp = /<[@#][a-f0-9]{32}>/
+function parseCommand(text) {
+  if (!text.startsWith(command)) return null
+  const args = text.substring(command.length)
+  const targetChannelPosition = args.search(channelRegexp)
+  if (targetChannelPosition === -1) return null
+  return {
+    channelId: args.substring(targetChannelPosition + 2, 34),
+    text: args.replace(channelRegexp, '').trim()
+  }
+}
+// </mess>
