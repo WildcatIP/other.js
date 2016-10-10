d7f709557da733d69427bbb09112b52c70f4acdd
Tony Gentilcore <tony@other.xyz>
2016-10-10 16:28:27 -0700

    Force Promise polyfill to be used always.
    This is to work around a suspected crash in the native Promise implementation in
    iOS 10's JSContext. Depending on whether this actually fixes the issue, we might
    be able to find a narrower workaround.
    
    It requires the iOS embedder to provide a `setTimeout` implementation along the
    lines of http://stackoverflow.com/a/30843832.
    
    Fixes #79

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Copyright 2016 Post Social, inc.
 
+// TODO: The native Promise implementation is crashy in iOS 10. This appears to
+// be fixed in the first service pack, but for now we clear the global Promise
+// so that the polyfill is always used.
+Promise = require('es6-promise').Promise  // eslint-disable-line no-native-reassign
+
 import 'babel-polyfill'
 import CommandListener from './lib/CommandListener'
 import Feature from './lib/Feature'
