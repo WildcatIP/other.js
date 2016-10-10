b67ba3f9d600995665c0916caef96f5566f5c93d
Tony Gentilcore <tony@other.xyz>
2016-10-10 16:41:14 -0700

    Fix build.
    Ref #79

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Copyright 2016 Post Social, inc.
 
-// TODO: The native Promise implementation is crashy in iOS 10. This appears to
-// be fixed in the first service pack, but for now we clear the global Promise
-// so that the polyfill is always used.
-Promise = require('es6-promise').Promise  // eslint-disable-line no-native-reassign
+if (global.__forcePromisePolyfill) {
+  // TODO: The native Promise implementation is crashy in iOS 10. This appears to
+  // be fixed in the first service pack, but for now we clear the global Promise
+  // so that the polyfill is always used.
+  Promise = require('es6-promise').Promise  // eslint-disable-line no-native-reassign
+}
 
 import 'babel-polyfill'
 import CommandListener from './lib/CommandListener'
