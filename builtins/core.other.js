0ebc990c2e02e50f13ca8b1e6e02a73fac3764d2
Juan-Carlos Foust <jc@theother.io>
2016-11-04 17:16:30 -0700

    update formats  (#98)
    * use 'quote' and 'small'
    
    * add 'system' format support
    
    * preserve alphabetical order

--- builtins/core.other.js
+++ builtins/core.other.js
@@ -10,10 +10,9 @@ const feature = new Feature({
 
 // Format commands
 feature.listen({
-  to: {commands: ['blockquote', 'caption', 'code', 'h1', 'h2', 'h3', 'p', 'small']},
+  to: {commands: ['caption', 'code', 'h1', 'h2', 'h3', 'p', 'quote', 'small', 'system']},
   on({command, args}) {
-    // TODO: Remove this small -> system hack once the iOS client understands small.
-    return {stagedMessage: {format: command === 'small' ? 'system' : command}}
+    return {stagedMessage: {format: command}}
   },
 })
 
