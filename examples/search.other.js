51e52bd1df28ea707ca3592a645db6d3266292f1
Tony Gentilcore <tony@other.xyz>
2016-11-30 14:45:40 -0800

    Fix search feature.
    The URL is https now.

--- examples/search.other.js
+++ examples/search.other.js
@@ -2,7 +2,7 @@ const {fetch, Feature} = require('other')
 
 const feature = new Feature({
   name: 'Duck Duck Go',
-  version: '0.0.1',
+  version: '0.0.2',
   dependencies: {
     otherjs: '^3.2.x',
   },
@@ -12,7 +12,7 @@ feature.listen({
   to: {words: ['web']},
   on({word, rest}) {
     const query = encodeURIComponent(rest.replace(word, '').trim())
-    const url = `http://api.duckduckgo.com/?q=${query}&format=json`
+    const url = `https://api.duckduckgo.com/?q=${query}&format=json`
     return fetch(url).then((response) => response.json()).then((json) => {
       if (!json.RelatedTopics) return null
       const chatCompletions = json.RelatedTopics.map((t) => {
