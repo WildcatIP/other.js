3c0a0fa6eff77ca63d19d7b17f7423737d407dbc
Tony Gentilcore <tony@other.xyz>
2016-12-07 05:51:53 -0800

    A stub for a feature host.
    Currently this just has some metadata getters. A pattern for handling some
    events is next.
    
    Ref #55, #111

---
+++ embedder.js
@@ -0,0 +1,11 @@
+if (global.__forcePromisePolyfill) {
+  // TODO: The native Promise implementation is crashy in iOS 10. This appears to
+  // be fixed in the first service pack, but for now we clear the global Promise
+  // so that the polyfill is always used.
+  Promise = require('es6-promise').Promise  // eslint-disable-line no-global-assign, no-native-reassign
+}
+
+import 'babel-polyfill'
+import FeatureHost from './lib/embedder/FeatureHost'
+
+export {FeatureHost}
